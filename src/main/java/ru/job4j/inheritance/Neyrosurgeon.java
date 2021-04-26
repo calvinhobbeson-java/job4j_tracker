ef6284183319710d2038d530e9bec542a050cf50
Ruviel21 <calvinhobbeson.java@gmail.com>
2021-04-26 22:29:38 +0300

    3. Реализация профессий в коде [#497573]

--- src/main/java/ru/job4j/inheritance/Neyrosurgeon.java
+++ src/main/java/ru/job4j/inheritance/Neyrosurgeon.java
@@ -4,7 +4,6 @@ public class Neyrosurgeon extends Doctor {
 
     public Neyrosurgeon(String name, String surname, String education, String birthday, String diagnosis) {
         super(name, surname, education, birthday, diagnosis);
-        diagnosis = "Brain damage, gonna make a lobotomy or smth";
     }
 
     public  String healSomeSeriousBrainShit() {
