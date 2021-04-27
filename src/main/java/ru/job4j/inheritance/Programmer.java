f5db79ed5e7f63759adb98a180d30b564409e4ad
Ruviel21 <calvinhobbeson.java@gmail.com>
2021-04-27 20:15:26 +0300

    3. Реализация профессий в коде [#497573]

--- src/main/java/ru/job4j/inheritance/Programmer.java
+++ src/main/java/ru/job4j/inheritance/Programmer.java
@@ -5,7 +5,6 @@ public class Programmer extends Engineer {
 
     public Programmer(String name, String surname, String education, String birthday, int calculation) {
         super(name, surname, education, birthday, calculation);
-        calculation = 11;
     }
 
     public int pushButtons() {
