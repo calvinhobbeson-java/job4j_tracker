8d9d1542c28e319c9ea293e4a1722e9083c3cd99
Ruviel21 <calvinhobbeson.java@gmail.com>
2021-05-04 13:24:32 +0300

    3. Интерфейс Input [#497507]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,14 +1,22 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport {
+    private boolean hasToGo = false;
 
     public void drive() {
+        if (hasToGo) {
+            boolean driveBus = true;
+        }
     }
 
     public void passengers(int passengers) {
+        if (passengers > 0) {
+            hasToGo = true;
+        }
     }
 
     public int refuel(int fuelCount) {
-        return -1;
+        int price = (100 - fuelCount) * 50;
+        return price;
     }
 }
