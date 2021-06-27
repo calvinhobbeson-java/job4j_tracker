b56ee517dfcff47c967e901137fa593587693212
Ruviel21 <calvinhobbeson.java@gmail.com>
2021-06-27 23:02:28 +0300

    1.2. Функциональный интерфейс [#247497 #169738]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -1,9 +1,6 @@
 package ru.job4j.function;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.*;
 
 public class FunctionalInterfaces {
