ee91f904448b4dc44c6af69f9e4fe7414e9fb5a1
Ruviel21 <calvinhobbeson.java@gmail.com>
2021-07-03 21:01:36 +0300

    0. Stream API [#58166]

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,17 @@
+package ru.job4j.stream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static void main(String[] args) {
+        List<Integer> ints = new ArrayList<>();
+        ints.add(-1);
+        ints.add(-4);
+        ints.add(2);
+        ints.add(-1);
+        ints.add(1);
+        List<Integer> positive = ints.stream().filter(i -> i > 0).collect(Collectors.toList());
+        positive.forEach(System.out::println);
+    }
+}
